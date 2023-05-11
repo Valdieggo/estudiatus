2ae26147c7d348a0d67768672c059ce9fba29301
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-05-11 01:00:59 -0400

    fix db util

--- utils/db.js
+++ utils/db.js
@@ -8,7 +8,6 @@ export async function connectToDatabase() {
     return mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
     });
 }
 
