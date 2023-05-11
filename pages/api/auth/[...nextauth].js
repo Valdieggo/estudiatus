10de55f5ae7761cdbbd98134228c0f5a90b07115
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-05-10 23:08:11 -0400

    add next auth configuration

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,70 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { connectToDatabase } from "../../../utils/mongodb";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import User from "../../../models/User";
+import { verifyPassword } from "../../../utils/auth";
+
+export default  async function auth(req, res) {
+    await connectToDatabase();
+
+    return NextAuth(req, res, {
+        session: {
+            jwt: true,
+        },
+        providers: [
+            Providers.Credentials({
+                name: "Credentials",
+                credentials: {
+                    email: {
+                        label: "Email",
+                        type: "email",
+                        placeholder: "alumno@universidad.com",
+                    },
+                    password: {
+                        label: "Password",
+                        type: "password",
+                    },
+                },
+                async authorize(credentials) {
+                    const user = await User.findOne({ email: credentials.email });
+
+                    if (!user) {
+                        throw new Error("No user found");
+                    }
+
+                    const isValid = await verifyPassword(credentials.password, user.password);
+
+                    if (!isValid) {
+                        throw new Error("Could not log you in");
+                    }
+
+                    return { email: user.email, username: user.username, id: user._id };
+                },
+            }),
+        ],
+        adapter: MongoDBAdapter({
+            db: process.env.MONGODB_URI,
+            clientPromise: connectToDatabase(),
+        }),
+        pages: {
+            signIn: "/auth/signin",
+            signOut: "/auth/signout",
+            error: "/auth/error",
+        },
+        callbacks: {
+            async jwt(token, user) {
+                if (user) {
+                    token.id = user._id;
+                    token.username = user.username;
+                }
+                return token;
+            },
+            async session(session, token) {
+                session.user.id = token.id;
+                session.user.username = token.username;
+                return session;
+            }
+        }
+    });
+}
