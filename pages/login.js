32cc40e1dec89e16a7ba10ac5ccaa6f393755085
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-05-15 23:44:46 -0400

    change login page styles

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,87 @@
 
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Divider,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+
 import LoginForm from "../components/LoginForm";
+import { Link } from '@chakra-ui/next-js'
 
 const Login = () => {
   return (
-    <Box minH="100vh" display="flex" alignItems="center" justifyContent="center">
-      <LoginForm />
-    </Box>
+    <Container
+    maxW="lg"
+    py={{
+      base: '12',
+      md: '24',
+    }}
+    px={{
+      base: '0',
+      sm: '8',
+    }}
+  >
+    <Stack spacing="8">
+      <Stack spacing="6">
+        <Stack
+          spacing={{
+            base: '2',
+            md: '3',
+          }}
+          textAlign="center"
+        >
+          <Heading
+            size={{
+              base: 'xs',
+              md: 'sm',
+            }}
+          >
+            Bienvenido, inicia sesión.
+          </Heading>
+          <HStack spacing="1" justify="center">
+            <Text color="muted">No tienes una cuenta?</Text>
+            <Link href='/register' colorScheme="blue">
+              Registrate
+            </Link>
+          </HStack>
+        </Stack>
+      </Stack>
+      <Box
+        py={{
+          base: '0',
+          sm: '8',
+        }}
+        px={{
+          base: '4',
+          sm: '10',
+        }}
+        bg={{
+          base: 'transparent',
+          sm: 'bg-surface',
+        }}
+        boxShadow={{
+          base: 'none',
+          sm: 'md',
+        }}
+        borderRadius={{
+          base: 'none',
+          sm: 'xl',
+        }}
+      >
+        <LoginForm />
+      
+      </Box>
+    </Stack>
+  </Container>
   );
 };
 
