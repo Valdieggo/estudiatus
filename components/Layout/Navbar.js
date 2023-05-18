0f4e536e380b82ec73bb1120b988fd0c5f197fc5
Valdieggo <diego.valdera98@gmail.com>
2023-05-17 22:40:20 -0400

    Cambio colores

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,32 +1,50 @@
-import { Flex, Box, Link, Spacer, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
+import {
+  Flex,
+  Box,
+  Link,
+  Spacer,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
     <Flex bg="teal.500" p="4" color="white">
-      <Box p="2">
-        Nombre de la aplicación
-      </Box>
+      <Box p="2">Nombre de la aplicación</Box>
       <Spacer />
       <Box>
-        <Link px="4" href="/">Inicio</Link>
-        <Link px="4" href="/about">Acerca de</Link>
-        <Link px="4" href="/contact">Contacto</Link>
+        <Link px="4" href="/">
+          Inicio
+        </Link>
+        <Link px="4" href="/about">
+          Acerca de
+        </Link>
+        <Link px="4" href="/contact">
+          Contacto
+        </Link>
       </Box>
       <Spacer />
       <Box>
         <Menu>
-          <MenuButton as="button">
-            Usuario
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Perfil</MenuItem>
-            <MenuItem>Configuración</MenuItem>
-            <MenuItem>Salir</MenuItem>
+          <MenuButton as="button">Usuario</MenuButton>
+          <MenuList color="#67686B">
+            <MenuGroup title="Perfil" color="#000">
+              <MenuItem>Perfil</MenuItem>
+              <MenuItem>Configuración</MenuItem>
+            </MenuGroup>
+            <MenuDivider />
+            <MenuGroup title="Auth" color="#000">
+              <MenuItem>Salir</MenuItem>
+            </MenuGroup>
           </MenuList>
         </Menu>
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
