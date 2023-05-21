e4fe719cb794cdb1da2ec8a0833821282911dcba
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-05-20 21:23:07 -0400

    test session

--- components/Layout/User.js
+++ components/Layout/User.js
@@ -1,9 +1,12 @@
 import { useSession } from "next-auth/react"
 export default function User() {
-    const { data: session, status } = useSession()
+    const { data: session, status, } = useSession()
 
     if (status === "authenticated") {
-      return <p>Signed in as {session.token.email}</p>
+      return (<p>Signed in as {JSON.stringify(session.user.name)}</p>
+      
+      )
+
     }
   
     return <a href="/api/auth/signin">Sign in</a>
