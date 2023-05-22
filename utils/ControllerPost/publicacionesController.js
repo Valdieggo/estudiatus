b823053079bf41675e5eef2378281382c218a2aa
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-05-22 19:26:07 -0400

    Update Controller

--- utils/ControllerPost/publicacionesController.js
+++ utils/ControllerPost/publicacionesController.js
@@ -4,31 +4,31 @@ const publicacionQuery = {
     publicaciones: async () => {
         return await Publicacion.find()
     },
-    buscarPublicacionId : async (args) => {
+    searchPublicacionId : async (args) => {
         const publicacion = await Publicacion.findById(args.id);
         return publicacion;
     },
-    buscarPublicacionTitulo : async (args) => {
+    searchPublicacionTitulo : async (args) => {
         const publicacion = await Publicacion.findOne({titulo: args.titulo});
         return publicacion;
     },
-    buscarPublicacionUser: async (args) => {
+    searchPostUser: async (args) => {
         const publicacion = await Publicacion.findOne({titulo: args.User});
         return publicacion;
     },
-    buscarPublicacionSubject: async (args) => {
+    searchPostSubject: async (args) => {
         const publicacion = await Publicacion.findOne({titulo: args.subject});
         return publicacion;
     },
-    buscarPublicacionFecha: async (args) => {
+    searchPostDate: async (args) => {
         const publicacion = await Publicacion.findOne({titulo: args.createDate});
         return publicacion;
     },
-    buscarPublicacionScore: async (args) => {
+    searchPostScore: async (args) => {
         const publicacion = await Publicacion.findOne({titulo: args.score});
         return publicacion;
     },
-    all_publicaciones : async () => {
+    all_post : async () => {
         const publicaciones = await Publicacion.find({});
         return publicaciones;
     }
