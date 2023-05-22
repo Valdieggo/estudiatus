d94a83a30c2c8df1fe2d2a4bab4334234c5522cf
gaanno <gabriel.norambuenam@gmail.com>
2023-05-21 23:53:18 -0400

    Added models

---
+++ models/Subject.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const SubjectSchema = new mongoose.Schema({
+    subjectName: {
+        type: String,
+        required: [true, "Please provide a subject name"],
+        maxLength: [100, "Subject name cannot be more than 100 characters"],
+    },
+    description: {
+        type: String,
+        required: [true, "Please provide a description"],
+        minLength: [10, "Description cannot be less than 10 characters"],
+        maxLength: [300, "Subject description cannot be more than 300 characters"],
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    subscribers: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "User",
+    },
+    posts: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Post",
+    },
+    career: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Career",
+    }
+})
+
+module.exports =  mongoose.model("Subject", SubjectSchema);
