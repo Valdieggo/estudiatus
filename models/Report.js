9e45e11b3581d02a4cb68452a1429887b15f6390
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-05-23 21:05:30 -0400

    create shema report

---
+++ models/Report.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ReportSchema = new mongoose.Schema({
+    reason : {
+        type: String,
+        required: [true, "Please provide a reason"],
+        maxLength: [100, "Reason cannot be more than 100 characters"],
+    },
+    description : {
+        type: String,
+        required: [true, "Please provide a description"],
+        minLength: [100, "Description cannot be less than 10 characters"],
+        maxLength: [300, "Description cannot be more than 300 characters"],
+    },
+    reportUser : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    reportedUser : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    post : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        required: false,
+    },
+
+});
+
+module.exports =  mongoose.models.Report || mongoose.model("Report", ReportSchema);
+
+
