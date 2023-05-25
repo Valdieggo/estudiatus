3294f9c0871cb865ace4b8d7c24155db5361216b
gaanno <gabriel.norambuenam@gmail.com>
2023-05-25 01:45:45 -0400

    Updated college management

---
+++ pages/api/college/update/changeName.js
@@ -0,0 +1,37 @@
+// deberia ser un solo archivo para actualizar todo
+import { connectToDatabase } from "../../../../utils/db";
+import College from "../../../../models/College";
+
+export default async function handler(req, res) {
+    const { method } = req;
+    const { id, collegeName } = req.body;
+
+    if (!id || !collegeName) {
+        return res.status(400).json({ success: false, message: "No ID provideed" });
+    }
+
+    connectToDatabase();
+
+    if (await College.findOne({ collegeName })) {
+        return res.status(409).json({ sucess: false, message: "College already exists" });
+    }
+
+    switch (method) {
+        case "PUT":
+            try {
+                const query = await College.findByIdAndUpdate(id, {
+                    collegeName
+                });
+
+                return res.status(200).json({ success: true, message: query});
+            }
+            catch (error) {
+                return res.status(400).json({ success: false, message: error });
+            }
+        default:
+            return res.status(400).json({ success: false });
+
+    }
+}
+
+
