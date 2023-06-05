cf34571d739e6e8b9c0b77655081466f9a2f9b78
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-06-05 17:51:25 -0400

    create api for controllers

--- pages/api/report/reports.js
+++ pages/api/report/reports.js
@@ -1,60 +1,26 @@
-// create reports 
-import { connectToDatabase } from "../../../utils/db";
-import Report from "../../../models/Report";
-import User from "../../../models/User";
-
+import { createReport } from "../../../controllers/reportController";
+import { getReports } from "../../../controllers/reportController";
+import { updateReport } from "../../../controllers/reportController";
+import {deleteReport} from "../../../controllers/reportController";
 
 
 export default async function handler(req, res) {
-    await connectToDatabase();
-    const {reportUserId ,reportedUserId, reason, description, id} = req.body;
-    switch (req.method) {
+    const {method}=req;
+    
+    switch (method) {
         case "POST":
-            // Se crea el reporte OK
-            
-            const newReport = new Report({ reportUser: reportUserId, reportedUser:reportedUserId, reason, description });
-            await newReport.save();
-            if (newReport) {
-                return res.status(201).json({ message: "Report created" });
-            }
-            else {
-                return res.status(500).json({ message: "Report not created" });
-            }
+            await createReport(req,res);
+            break;
         case "GET":
-            //Se lisstna los reportes OK
-            const reports = await Report.find({}).populate("reportUser").populate("reportedUser").populate("post");
-            if (reports) {
-                return res.status(200).json({ reports });
-                // si report es igual a 0 entonces no hay reportes
-            }else if(reports == 0){
-                return res.status(200).json({ message: "No hay reportes" });
-            }else {
-                return res.status(500).json({ message: "Error al obtener los reportes" });
-            }
+            await getReports(req,res);
+            break;
         case "PUT":
-            // se actualiza el reporte segun lo que se le pase en el body OK
-            
-            const updateReport= await Report.findByIdAndUpdate(id, {
-                reportUser:reportUserId,
-                reportedUser:reportedUserId,
-                reason,
-                description},
-                  { new: true });
-            if (updateReport) {
-                return res.status(200).json({ message: "Report updated" });
-            }
-            else {
-                return res.status(500).json({ message: "Report not updated" });
-            }
+            await updateReport(req,res);
+            break;
         case "DELETE":
-            // se elimina el reporte segun lo que se le pase en el body
-            const deleteReport = await Report.findByIdAndDelete(id);
-            if (deleteReport) {
-                return res.status(200).json({ message: "Report deleted" });
-            }
-            else {
-                return res.status(500).json({ message: "Report not deleted" });
-            }
+            await deleteReport(req,res);
+            break;
+         
         default:
             res.status(400).json({ success: false});
             break;
