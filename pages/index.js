57e06a641811d8e2488018eabeedef2e4f50d49b
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-06-07 10:05:16 -0400

    delete User component in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
-import User from "../components/Layout/User";
+
 export default  function  Home() {
   return (
     <>
     <Layout>
-        <User />
+      Hola hola
     </Layout>
     </>
 
