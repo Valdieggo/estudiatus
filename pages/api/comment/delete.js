b78d3adf0e4297a7cb3c2949027e029e287f7e4d
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-06-07 11:04:26 -0400

    add update and delete comment api

---
+++ pages/api/comment/delete.js
@@ -0,0 +1,49 @@
+import { connectToDatabase } from "../../../utils/db";
+import Comment from "../../../models/Comment";
+import Post from "../../../models/Post";
+
+export default async function handler(req, res) {
+    const { method } = req;
+
+    connectToDatabase();
+
+    switch (method) {
+        case "DELETE":
+            try {
+                const { id } = req.body;
+
+                if (!id) {
+                    return res.status(400).json({ success: false, message: "Missing fields" });
+                }
+                
+                const comment = await Comment.findById(id);
+
+                if (!comment) {
+                    return res.status(400).json({ success: false, message: "Comment not found" });
+                }
+
+                const post = await Post.findById(comment.post);
+
+                if (!post) {
+                    return res.status(400).json({ success: false, message: "Post not found" });
+                }
+
+                post.comments.pull(id);
+                await post.save();
+
+
+                await Comment.findByIdAndDelete(id);
+
+                return res.status(200).json({ success: true, data: id });
+
+            }
+            catch (error) {
+                return res.status(400).json({ success: false, message: error });
+            }
+
+        default:
+            return res.status(400).json({ success: false });
+
+    }
+}
+
