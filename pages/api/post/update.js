ca5336b96c034ee63080ebe41547d67bdcc7326a
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-06-11 19:22:42 -0400

    Update postDiscuss

---
+++ pages/api/post/update.js
@@ -0,0 +1,20 @@
+import { connectToDatabase } from "../../../utils/db";
+
+import Post from "../../../models/Post";
+
+export default async function handler(req, res) {
+    const { method } =req;
+    await connectToDatabase();
+
+    switch (method) {
+        case "PUT":
+            try {
+                const posts = await Post.updateOne({})
+                return res.status(200).json({ success: true, data: posts });
+            } catch(error){
+                return res.status(400).json({ success: false, message: error });
+            }
+        default:
+            return res.status(400).json({ success: false });
+        }
+}
