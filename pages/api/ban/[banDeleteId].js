6ca4679b6835b9bf2e1b28a0346dafb0a2bacb89
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-06-12 22:08:25 -0400

    change to delete

---
+++ pages/api/ban/[banDeleteId].js
@@ -0,0 +1,31 @@
+import { connectToDatabase } from "../../../utils/db";
+import Ban from "../../../models/Ban";
+
+
+export default async function handler(req, res) {
+   try {
+   connectToDatabase();
+    const { method } = req;
+    const { BanDeleteId } = req.query;
+    console.log(BanDeleteId);
+
+
+    switch (method) {
+        case "DELETE":
+            const report = await Ban.findByIdAndDelete(BanDeleteId);
+            if (report) {
+                return res.status(200).json({ message: "Ban deleted" });
+            }
+            else {
+                return res.status(500).json({ message: "Ban not deleted" });
+            }
+
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+    } catch (error) {
+        return res.status(500).json({ message: "Error al eliminar el reporte" + error });
+    }
+
+};
