e59db88effd5b060fc2a1d8f69c0d9b785ee0d87
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-06-12 22:06:11 -0400

    report Crud ok

--- pages/api/report/[reportDeleteId].js
+++ pages/api/report/[reportDeleteId].js
@@ -6,11 +6,11 @@ export default async function handler(req, res) {
    try {
    connectToDatabase();
     const { method } = req;
-    const { deleteId } = req.query;
+    const { reportDeleteId } = req.query;
 
     switch (method) {
         case "DELETE":
-            const report = await Report.findByIdAndDelete(deleteId);
+            const report = await Report.findByIdAndDelete(reportDeleteId);
             if (report) {
                 return res.status(200).json({ message: "Report deleted" });
             }
