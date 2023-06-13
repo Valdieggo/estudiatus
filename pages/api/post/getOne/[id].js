03f4ba34b355189be6c776e96d230cffdf464982
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-06-13 16:07:27 -0400

    Update Post

---
+++ pages/api/post/getOne/[id].js
@@ -0,0 +1,26 @@
+import { connectToDatabase } from "../../../utils/db";
+
+import Post from "../../../models/Post";
+
+export default async function handler(req, res) {
+    const { method } = req;
+    const { id } = req.query;
+
+    await connectToDatabase();
+
+    switch (method) {
+        case "GET":
+            try {
+                const post = await Post.findById(id);
+                if (!post) {
+                    return res.status(400).json({ success: false, message: "Post not found" });
+                }
+                return res.status(200).json({ success: true, data: post });
+            }
+            catch (error) {
+                return res.status(400).json({ success: false, message: error });
+            }
+        default:
+            return res.status(400).json({ success: false,message: error });
+    }
+}
