0a4708c3bede847754cf3cf64da64b507e6b5c59
Gaanno <gabriel.norambuenam@gmail.com>
2023-06-13 23:36:08 -0400

    cambios en el diseño

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,10 @@
 import Sidebar from "./SideBar"
 import Navbar from "./Navbar";
-import { Box, Container, Flex, Stack } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/layout";
+import { Box, Drawer, Flex, Heading, Text } from '@chakra-ui/react';
 
 function Layout({ children }) {
+    /*
     return (
         <Container
             bg="bg.100"
@@ -20,7 +22,65 @@ function Layout({ children }) {
                 <Box>{children}</Box>
             </Flex>
         </Container>
-    );
+    ); 
+
+    return (
+        <Grid
+            templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+            gridTemplateRows={'50px 1fr 30px'}
+            gridTemplateColumns={'150px 1fr'}
+            h='200px'
+            gap='1'
+            color="white"
+            fontWeight='bold'
+            bg ="bg.100"
+        >
+            <GridItem pl='' area={'header'}>
+                <Navbar />
+            </GridItem>
+            <GridItem pl='2'  area={'nav'}>
+                <Sidebar />
+            </GridItem>
+            <GridItem pl='2'  area={'main'}>
+                {children}
+            </GridItem>
+            <GridItem pl='2' area={'footer'}>
+                Footer
+            </GridItem>
+        </Grid>
+
+    )*/
+
+    return (
+        <Box bg="bg.100"
+            color="white">
+            <Grid
+                templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+                gridTemplateRows={'50px 1fr 30px'}
+                gridTemplateColumns={'200px 1fr'}
+                h="100vh"
+                gap='1'
+                fontWeight='bold'
+            >
+                <GridItem pl='2'  area={'header'}>
+                    <Navbar />
+                </GridItem>
+                <GridItem pl='2' area={'nav'}>
+                    <Sidebar />
+                </GridItem>
+                <GridItem pl='2'  area={'main'} display="block">
+                    {children}
+                </GridItem>
+                <GridItem pl='2'  area={'footer'}>
+                    Footer
+                </GridItem>
+            </Grid>
+        </Box>
+    )
 }
 
 export default Layout;
--- components/Layout/SideBar.js
+++ components/Layout/SideBar.js
@@ -1,8 +1,10 @@
 import { Box, Stack, Link, Text } from '@chakra-ui/react'
 import Administrator from '../Admin/Administrator'
-
+import {Drawer} from '@chakra-ui/react'
 const Sidebar = () => {
-  return (
+ /**
+  * 
+  *  return (
     <Box
       w="200px"
       h="100vh"
@@ -28,6 +30,43 @@ const Sidebar = () => {
       </Stack>
     </Box>
   )
+  */
+ return (
+  <Drawer
+  isOpen={true}
+  placement="left"
+  onClose={() => {}}
+  size="xs"
+  bg="gray.800"
+  >
+    <Box
+      w="200px"
+      h="100vh"
+      position="fixed"
+      top={0}
+      left={0}
+      bg="gray.800"
+      color="white"
+      p="5"
+    >
+       <Text fontSize="2xl" mb="5">
+        Menú
+      </Text>
+      <Stack spacing="3">
+        <Link href="/">Inicio</Link>
+        <Link href="/about">Acerca de</Link>
+        <Link href="/contact">Contacto</Link>
+        <Link href="/college">Universidades con un titulo largo para ver que pasa</Link>
+
+      </Stack>
+      <Stack spacing="3">
+      {Administrator()}
+      </Stack>
+    </Box>
+  </Drawer>
+
+
+ )
 }
 
 export default Sidebar
