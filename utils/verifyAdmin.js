dd869fa0cb392a1f3004fcbb30fc7681b3680db6
Gaanno <gabriel.norambuenam@gmail.com>
2023-06-14 04:30:52 -0400

    Arreglo errores comprobacion admin

--- utils/verifyAdmin.js
+++ utils/verifyAdmin.js
@@ -1,28 +1,29 @@
-import isAdmin from "./isAdmin"
 import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react";
+
 
 // This function is used to verify if the user is an admin
 // If the user is not an admin, it will redirect to the home page
 export default function verifyAdmin() {
     const { data: session, status } = useSession();
 
-    //espera a que cargue para verificar
-    if (status === "loading") {
-        return false;
-    }
-
-    if (typeof window !== 'undefined') {
-        if(status === "authenticated" ){
-
-            if(session.user.role === "admin"){
-                return true;
-            }else{
-                window.location.href = "/"
+    const verify = () => {
+        if (typeof window !== 'undefined') {
+            if (status === "authenticated") {
+                if (session.user.role === "admin") {
+                    return;
+                } 
             }
-        }
-        if (status !== "authenticated" || session.user.role !== "admin") {
             window.location.href = "/"
         }
     }
-    return false;
+
+    useEffect(() => {
+        console.log("estado: ", status)
+        if(status !== "loading"){
+            verify();
+        }
+    }, [
+        status === "loading"
+    ]);
 }
