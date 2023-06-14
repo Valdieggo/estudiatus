0c8e884cf0c48eb490a31180691ca3ccab0b5160
Gaanno <gabriel.norambuenam@gmail.com>
2023-06-13 20:48:52 -0400

    verificacion admin

---
+++ utils/verifyAdmin.js
@@ -0,0 +1,28 @@
+import isAdmin from "./isAdmin"
+import { useSession } from "next-auth/react"
+
+// This function is used to verify if the user is an admin
+// If the user is not an admin, it will redirect to the home page
+export default function verifyAdmin() {
+    const { data: session, status } = useSession();
+
+    //espera a que cargue para verificar
+    if (status === "loading") {
+        return false;
+    }
+
+    if (typeof window !== 'undefined') {
+        if(status === "authenticated" ){
+
+            if(session.user.role === "admin"){
+                return true;
+            }else{
+                window.location.href = "/"
+            }
+        }
+        if (status !== "authenticated" || session.user.role !== "admin") {
+            window.location.href = "/"
+        }
+    }
+    return false;
+}
