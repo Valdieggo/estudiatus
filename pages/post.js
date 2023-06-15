c173a958134a0c2920a4daccb703a86951fca919
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-06-14 20:30:14 -0400

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -8,10 +8,10 @@ const Post = () => {
     return (
         <>
             <Container minH='92vh' minW='74vw' maxW='74vw' centerContent overflow='hidden'>
-                <Heading textAlign={"center"} my={10}>Usuarios</Heading>
+                <Heading textAlign={"center"} my={10}>Publicaciones</Heading>
                 <Button colorScheme="teal" /* onClick={() => router.push('/posts/create')} */ >Nueva Publicacion</Button>
                 <FormControl>
-                <FormLabel>Buscar usuario</FormLabel>
+                <FormLabel>Buscar Publicacion</FormLabel>
                 <Input /* onChange={(e)=>getAll(e.target.value)} */ placeholder=" ingrese el Tema a buscar " type='text' />
                 </FormControl>
 
