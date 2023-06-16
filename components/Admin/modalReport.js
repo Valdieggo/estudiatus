5e41e9035a4e99e247e46fbc5e760a1a7c0cac73
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-06-15 20:19:31 -0400

    modal report whit pop over

--- components/Admin/modalReport.js
+++ components/Admin/modalReport.js
@@ -1,69 +1,74 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, ModalFooter, Text, Stack, Grid, Button, Spinner} from "@chakra-ui/react";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, ModalFooter, Text, Stack, Grid, Button, Spinner, Center } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { ConfirmationPopover } from "./PopBan"
+
 
 export default function ModalReport({ isOpen, onClose, reportId }) {
+  const [report, setReport] = useState(null);
+  const [showPopover, setShowPopover] = useState(false);
 
-    const [report, setReport] = useState(null);
-    
-
-    useEffect(() => {
-      const fetchReport = async () => {
-        try {
-          const response = await axios.get(`../api/report/getOne/${reportId}`);
-          setReport(response.data.report);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-  
-      if (isOpen && reportId) {
-        fetchReport();
+  useEffect(() => {
+    const fetchReport = async () => {
+      try {
+        const response = await axios.get(`../api/report/getOne/${reportId}`);
+        setReport(response.data.report);
+      } catch (error) {
+        console.error(error);
       }
-    }, [isOpen, reportId]);
+    };
 
-    const handlreClose = () => {
-        setReport(null);
-        onClose();
+    if (isOpen && reportId) {
+      fetchReport();
     }
+  }, [isOpen, reportId]);
 
+  const handleClose = () => {
+    setReport(null);
+    onClose();
+  };
 
+  const handleConfirmBan = () => {
+    console.log("Sancionado");
+    setShowPopover(false);
+  };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size={"xl"}  closeOnOverlayClick={false} >
-      <ModalOverlay backdropFilter='auto' backdropBlur='2px' />
+    <Modal isOpen={isOpen} onClose={onClose} size="xl" closeOnOverlayClick={false}>
+      <ModalOverlay backdropFilter="auto" backdropBlur="2px" />
       <ModalContent background="bg.100" color="white">
         <ModalHeader>Report Details</ModalHeader>
-        <ModalCloseButton/>
+        <ModalCloseButton background="red" onClick={handleClose} />
         <ModalBody>
-        {report ? (
+          {report ? (
             <>
-                <Stack spacing={1}>
-                    <Text fontWeight="bold">Reported User:</Text>
-                    <Grid templateColumns="auto 1fr" columnGap= "50%">
-                    <Text>{report.reportedUser.username}</Text>
-                    <Text>{report.reportedUser.email}</Text>
-                    </Grid>
-                    <Text fontWeight="bold">Reason:</Text>
-                        <Text>{report.reason}</Text>
-                    <Text fontWeight="bold">Description:</Text>
-                        <Text>{report.description}</Text>
-
-                </Stack>
+              <Stack spacing={1}>
+                <Text fontWeight="bold">Usuario reportado:</Text>
+                <Grid templateColumns="auto 1fr" columnGap="50%">
+                  <Text>{report.reportedUser.username}</Text>
+                  <Text>{report.reportedUser.email}</Text>
+                </Grid>
+                <Text fontWeight="bold">Razon:</Text>
+                <Text>{report.reason}</Text>
+                <Text fontWeight="bold">Descripcion:</Text>
+                <Text>{report.description}</Text>
+              </Stack>
             </>
           ) : (
             <Stack spacing={4} alignItems="center">
               <Spinner size="xl" />
-              <Text>Loading...</Text>
+              <Text>Cargando...</Text>
             </Stack>
           )}
         </ModalBody>
-        <ModalFooter>
-            <Button colorScheme="green" mr={3} onClick={handlreClose}>
-                Close
-            </Button>
-
-
+        <ModalFooter justifyContent="center">
+            <ConfirmationPopover
+              message="¿Estás seguro de que deseas sancionar?"
+              onConfirm={handleConfirmBan}
+            />
+          <Button colorScheme="green" mx={10} onClick={handleClose}>
+            No sancionar
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
