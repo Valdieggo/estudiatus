3fabd1758918dd2f5b312d86b68aed3cb936f821
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-06-21 18:15:19 -0400

    User for id [Del,GetOne]

---
+++ pages/api/user/getOne/[id].js
@@ -0,0 +1,26 @@
+import { connectToDatabase } from "../../../../utils/db";
+import User from "../../../../models/user";
+
+
+export default async function handler(req, res) {
+    const { method } = req;
+    const { id } = req.query;
+
+    await connectToDatabase();
+
+    switch (method) {
+        case "GET":
+            try {
+                const user = await User.findById(id)
+                if (!user) {
+                    return res.status(400).json({ success: false, message: "user not found" });
+                }
+                return res.status(200).json({ success: true, data: user });
+            }
+            catch (error) {
+                return res.status(400).json({ success: false, message: error });
+            }
+        default:
+            return res.status(400).json({ success: false,message: error });
+    }
+}
