c5abba7e1f0c840ea0991b6a9b498110d166f7c8
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-07-20 15:54:22 -0400

    Update [id].js

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -3,8 +3,10 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import CommentCard from "../../components/Comment/CommentCard";
-import { VStack } from "@chakra-ui/react";
+import { VStack,Text,CardBody,IconButton,Box,Heading,Flex,Card,CardHeader,Image,CardFooter,Button,Avatar} from "@chakra-ui/react";
 import AddCommentCard from "../../components/Comment/AddCommentCard";
+import { BiLike, BiChat, BiShare } from "react-icons/bi";
+import {BsThreeDotsVertical} from "react-icons";
 
 export default function Post({ post }) {
     const router = useRouter();
@@ -15,12 +17,59 @@ export default function Post({ post }) {
     return (
         <Layout>
             <h1>Post: {post.title}</h1>
-            <p>Esto se deberia cambiar por el componente PostCard</p>
             <VStack spacing={4} align="center">
-                <AddCommentCard post={post} setComments={setComments} comments={comments}/>
+            <Card maxW='md'>
+                <CardHeader>
+                    <Flex spacing='4'>
+                        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                            <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+
+                            <Box>
+                                <Heading size='sm'>Segun Adebayo</Heading>
+                                <Text>Creator, Chakra UI</Text>
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </CardHeader>
+                <CardBody>
+                    <Text>
+                        With Chakra UI, I wanted to sync the speed of development with the speed
+                        of design. I wanted the developer to be just as excited as the designer to
+                        create a screen.
+                    </Text>
+                </CardBody>
+                <Image
+                    objectFit='cover'
+                    src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    alt='Chakra UI'
+                />
+
+                <CardFooter
+                    justify='space-between'
+                    flexWrap='wrap'
+                    sx={{
+                        '& > button': {
+                            minW: '136px',
+                        },
+                    }}
+                >
+                    <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
+                        Like
+                    </Button>
+                    <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
+                        Comment
+                    </Button>
+                    <Button flex='1' variant='ghost' leftIcon={<BiShare />}>
+                        Share
+                    </Button>
+                </CardFooter>
+            </Card>
+            </VStack>
+            <VStack spacing={4} align="center">
+                <AddCommentCard post={post} setComments={setComments} comments={comments} />
                 {comments && comments.map((comment) => (
-                <CommentCard key={comment._id} comment={comment} setComments={setComments} comments={comments}/>
-            ))}
+                    <CommentCard key={comment._id} comment={comment} setComments={setComments} comments={comments} />
+                ))}
             </VStack>
         </Layout>
     );
@@ -30,7 +79,7 @@ export async function getServerSideProps(context) {
     const { id } = context.query;
     const res = await fetch(`http://localhost:3000/api/post/getOne/${id}`);
     const data = await res.json();
-    if(res.status === 400) {
+    if (res.status === 400) {
         return {
             notFound: true,
         };
