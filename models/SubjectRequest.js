0d80c95339a7441c95eba60518cd389dc9f4a06f
Valdieggo <diego.valdera98@gmail.com>
2023-07-25 03:28:13 -0400

    subject request model

---
+++ models/SubjectRequest.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const SubjectRequestSchema = mongoose.Schema({
+  subjectName: {
+    type: String,
+    minLenght: 1,
+    maxLenght: 100,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  requestingUser: {
+    type: mongoose.Schema.Types.ObjectID,
+    ref: "User",
+  },
+  description: {
+    type: String,
+    required: false,
+    minLength: 1,
+    maxLength: 1000,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending",
+  },
+});
+
+module.exports =
+  mongoose.models.SubjectRequest ||
+  mongoose.model("SubjectRequest", SubjectRequestSchema);
