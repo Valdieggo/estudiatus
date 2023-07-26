9061b671a8074c77ca7602dd02887965eda254bd
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-07-26 01:37:08 -0400

    Create a transaction for procces actualization

--- pages/api/ban/create.js
+++ pages/api/ban/create.js
@@ -1,5 +1,7 @@
 import { connectToDatabase } from "../../../utils/db";
 import Ban from "../../../models/Ban";
+import User from "../../../models/User";
+import Report from "../../../models/Report";
 
 
 export default async function handler(req, res) {
@@ -9,20 +11,28 @@ export default async function handler(req, res) {
 
     switch (method) {
         case "POST":
+            const session = await mongoose.startSession();
+
             try {
-                const newBan = new Ban({ user,type,time,status,report });
+                session.startTransaction();
+                // Crea el nuevo registro de ban y lo guarda en la base de datos
+                const newBan = new Ban({ user, type, time, status, report });
                 await newBan.save();
-                if (newBan) {
-                    // se retorna el ban creado
-                    return res.status(201).json({ message: "Ban created", ban: newBan} );
-                }
-                else {
-                    return res.status(500).json({ message: "Ban not created" });
-                }
-        
-            } catch (error) {
-                return res.status(500).json({ message: "Error al crear el ban" + error});
+                // Actualiza el usuario en el campo de isBanned
+                await User.findByIdAndUpdate(user, { isBanned: newBan._id }, { session });
+                // Actualiza el reporte en el campo de estatus a "solved"
+                const ReportUpdate = await Report.findByIdAndUpdate(report, { status: 'solved' }, { session });
+                await session.commitTransaction();
+                session.endSession();
+                // Si todo va bien, se devuelve el nuevo ban creado
+                return res.status(201).json({ success: true, data: ReportUpdate });
+                
+            }catch (error) {
+                await session.abortTransaction();
+                session.endSession();
+                throw error;
             }
+
         default:
             res.status(400).json({ success: false });
         break;
