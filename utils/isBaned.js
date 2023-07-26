0ea8a2b5289dc3cfbad2e1c1032ebaec4270021f
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-07-26 19:14:18 -0400

    banned for boolean

--- utils/isBaned.js
+++ utils/isBaned.js
@@ -3,24 +3,24 @@ import { connectToDatabase } from "./db";
 
 export const isBaned = async (reportedUser) => {
             // se busca el ban del usuario reportado en la base de datos cuando el estatus sea "active" y se guarda en la variable ban
-            connectToDatabase();
-            const ban = await Ban.findOne({ user: reportedUser, status: "active" });
-   
-                // si no hay ban se devuelve false
-                if (!ban) {
-                    return false;
-                }
-                // si esta activo se verifica que el tiempo de ban no haya expirado
-                const banTime = new Date(ban.time);
-                const now = new Date();
-                if (banTime > now) {
-                    // si el tiempo de ban no ha expirado se devuelve true
-                return true;
-                }else{
-                    // si el tiempo de ban ha expirado se actualiza el estatus del ban a "inactive"
-                    connectToDatabase();
-                    await Ban.findByIdAndUpdate(ban._id, { status: "inactive" });
-                    // se devuelve false
-                    return false;
-                }
+    connectToDatabase();
+    const ban = await Ban.findOne({ user: reportedUser, status: "active" });
+
+    // si no hay ban se devuelve false
+    if (!ban) {
+        return true;
+    }
+    // si esta activo se verifica que el tiempo de ban no haya expirado
+    const banTime = new Date(ban.time);
+    const now = new Date();
+    if (banTime > now) {
+        // si el tiempo de ban no ha expirado se devuelve true
+    return true;
+    }else{
+        // si el tiempo de ban ha expirado se actualiza el estatus del ban a "inactive"
+        connectToDatabase();
+        await Ban.findByIdAndUpdate(ban._id, { status: "inactive" });
+        // se devuelve false
+        return false;
+    }
 };
