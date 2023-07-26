5019b9a1967342c460889fc32d51ff3e39cbb582
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-07-26 00:51:52 -0400

    update likepost button para mostrar likes y feedback de cargando

--- components/Post/LikePostButton.js
+++ components/Post/LikePostButton.js
@@ -1,5 +1,5 @@
 // LikePostButton.js
-import { Button } from "@chakra-ui/react";
+import { Button, Spinner } from "@chakra-ui/react";
 import { ArrowUpIcon } from "@chakra-ui/icons";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -7,13 +7,15 @@ import { useSession } from "next-auth/react";
 import { useDisclosure } from '@chakra-ui/react'
 import LoginModal from "../Auth/LoginModal.js";
 
-export default function LikePostButton( { post } ) {
+export default function LikePostButton({ post }) {
     const { data: session, status } = useSession();
     const [isLiked, setIsLiked] = useState(false);
+    const [likeCount, setLikeCount] = useState(post.likes.length);
+    const [isLiking, setIsLiking] = useState(false);
     const { isOpen, onOpen, onClose } = useDisclosure()
 
     useEffect(() => {
-        if(status === "authenticated") {
+        if (status === "authenticated") {
             const userId = session.user.id;
             const postId = post._id;
             axios.post("/api/post/getUserLike", { userId, postId })
@@ -27,16 +29,22 @@ export default function LikePostButton( { post } ) {
     }, [status]);
 
     const handleLike = () => {
-        if(status === "authenticated") {
+        if (status === "authenticated") {
+            setIsLiking(true);
             const userId = session.user.id;
             const postId = post._id;
             console.log(userId, postId);
             axios.put("/api/post/like", { userId, postId })
                 .then((res) => {
-                    setIsLiked(res.data.data.includes(userId));
+                    const liked = res.data.data.includes(userId);
+                    setIsLiked(liked);
+                    // Increment or decrement the like count depending on if the post was liked or unliked
+                    setLikeCount((prevCount) => prevCount + (liked ? 1 : -1));
+                    setIsLiking(false);
                 })
                 .catch((err) => {
                     console.log(err);
+                    setIsLiking(false);
                 });
         } else {
             onOpen();
@@ -50,12 +58,14 @@ export default function LikePostButton( { post } ) {
             bg={buttonColor}
             width="48%"
             onClick={handleLike}
+            isDisabled={isLiking}
             _hover={{
                 bg: "button.200",
-            }} leftIcon={<ArrowUpIcon />}>
-            Like
+            }} 
+            leftIcon={isLiking ? <Spinner /> : <ArrowUpIcon />}>
+            Like {likeCount}
             <LoginModal isOpen={isOpen} onClose={onClose} />
         </Button>
-        
     );
 }
+
