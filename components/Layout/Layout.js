cbbb644290b6b7a39dc25f5e1668c15335dfeba8
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-07-26 01:43:22 -0400

    fix problemas menores layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -52,9 +52,13 @@ const Layout = ({ children, user }) => {
         document.body.style.backgroundColor = "#151f32";
     }, []);
   return (
-    <Box>
+    <Box
+        color="white"
+        w="100%"
+        h="100vh"
+    >
       <Navbar user={user} />
-      <Flex>
+      <Flex color="white">
         <Sidebar />
         <Box flex={1} p={5}>
           {children}
