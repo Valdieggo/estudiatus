296d9504fcd1610781213e73fc6337af57247261
Matias-UBB <87254204+Matias-UBB@users.noreply.github.com>
2023-07-28 22:53:45 -0400

    Pagina manejo de errores

--- pages/auth/error.js
+++ pages/auth/error.js
@@ -1,7 +1,52 @@
-im
+import { useEffect, useState } from "react";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import BannedModal from "../../components/Error/ModalBaned"; // Importa el componente de modal para usuarios baneados
+import ErrorModal from "../../components/Error/GeneralError"; // Importa el componente de modal para otros errores
+import Layout from "../../components/Layout/Layout";
+import { set } from "mongoose";
 
-export default function error (){
-  console.log(req.query);
+const BanError = () => {
+  const router = useRouter();
+  const { error } = router.query;
+  const [showBannedModal, setShowBannedModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [idBan, setIdBan] = useState(null);
 
+  useEffect(() => {
+    // Muestra el modal correspondiente en función del tipo de error
+    if (error) {
+      // Muestra el modal correspondiente en función del tipo de error
+      if (error.startsWith("BANNED/")) {
+        const banIdFromQuery = error.substring("BANNED/".length); // Extraer el ID del ban del query string
+        setIdBan(banIdFromQuery);
+        setShowBannedModal(true);
+      } else {
+        setShowErrorModal(true);
+      }
+    }
+   
+  }, [error]);
 
-}
+  const handleCloseModals = () => {
+    // Cierra ambos modales cuando el usuario hace clic en el botón de cerrar
+    setShowBannedModal(false);
+    setShowErrorModal(false);
+  };
+
+ 
+
+  return (
+    <Layout>
+    <Box>
+      {/* Modal para usuarios baneados */}
+      <BannedModal isOpen={showBannedModal} onClose={handleCloseModals} idBan={idBan} />
+
+      {/* Modal para otros errores */}
+      <ErrorModal isOpen={showErrorModal} onClose={handleCloseModals} errorMessage={error} />
+    </Box>
+    </Layout>
+  );
+};
+
+export default BanError;
