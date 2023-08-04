9947474d270c28604a86bfa3feb0ab3fd714c717
Valdieggo <diego.valdera98@gmail.com>
2023-08-04 18:21:56 -0400

    subject changes

--- pages/subjectRequest.js
+++ pages/subjectRequest.js
@@ -30,60 +30,63 @@ export default function SubjectRequest() {
 
     return (
         <Layout>
-            <Box direction="column" align="center">
+            <Box direction="column" align="center" pb={20}>
                 <Text>Solicitar nueva asignatura</Text>
             </Box>
-            <VStack
-                as="form"
-                onSubmit={handleSubmit(submitRequest)}
-                pr={100}
-                pl={100}
+            <Box
+                p={7}
+                maxWidth="500px"
+                borderWidth={1}
+                borderRadius={8}
+                mx={"auto"}
             >
-                <FormControl pt={20}>
-                    <FormLabel>Nombre de la asignatura</FormLabel>
-                    <Input
-                        type="text"
-                        {...register("subjectName", { required: true })}
-                    />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Universidad</FormLabel>
-                    <Select
-                        placeholder="Selecciona una Universidad"
-                        {...register("college", { required: true })}
+                <VStack as="form" onSubmit={handleSubmit(submitRequest)}>
+                    <FormControl>
+                        <FormLabel>Nombre de la asignatura</FormLabel>
+                        <Input
+                            type="text"
+                            {...register("subjectName", { required: true })}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Universidad</FormLabel>
+                        <Select
+                            placeholder="Selecciona una Universidad"
+                            {...register("college", { required: true })}
+                        >
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </Select>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Carrera</FormLabel>
+                        <Select
+                            placeholder="Selecciona una carrera"
+                            {...register("career", { required: true })}
+                        >
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </Select>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Descripcion</FormLabel>
+                        <Input
+                            type="text"
+                            {...register("description", { required: true })}
+                        />
+                    </FormControl>
+                    <Button
+                        type="submit"
+                        bg="button.100"
+                        width="100%"
+                        _hover={{
+                            bg: "button.200",
+                        }}
                     >
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                    </Select>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Carrera</FormLabel>
-                    <Select
-                        placeholder="Selecciona una carrera"
-                        {...register("career", { required: true })}
-                    >
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                    </Select>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Descripcion</FormLabel>
-                    <Input
-                        type="text"
-                        {...register("description", { required: true })}
-                    />
-                </FormControl>
-                <Button
-                    type="submit"
-                    bg="button.100"
-                    width="100%"
-                    _hover={{
-                        bg: "button.200",
-                    }}
-                >
-                    Enviar
-                </Button>
-            </VStack>
+                        Enviar
+                    </Button>
+                </VStack>
+            </Box>
         </Layout>
     );
 }
