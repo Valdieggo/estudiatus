278c308daa955ddd18882aa9c90c2485664827ae
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-08-04 14:53:21 -0400

    Orden de cracion en decendente (Recientes primero)

--- pages/api/post/getAllBysubject/[id].js
+++ pages/api/post/getAllBysubject/[id].js
@@ -1,8 +1,6 @@
 import { connectToDatabase } from "../../../../utils/db";
 import Post from "../../../../models/Post";
 import Subject from "../../../../models/Subject"
-import Comment from "../../../../models/Comment";
-
 
 export default async function handler(req, res) {
     const { method } = req;
@@ -14,31 +12,27 @@ export default async function handler(req, res) {
             try {
                 // Extract idSubjectAll from request parameters
                 const { id } = req.query;
-
                 // Check if idSubjectAll is present
                 if (!id) {
                     return res.status(400).json({ success: false, message: "idSubjectAll parameter missing" });
                 }
-
                 // Find the subject by its ID
                 const subject = await Subject.findById(id);
-
                 if (!subject) {
                     return res.status(400).json({ success: false, message: "Subject not found" });
                 }
-
                 // Find all posts that belong to the subject
-                const posts = await Post.find({ subject: subject._id }).populate(
-                    "creator").populate({
-                        path: "comments",
-                        model: "Comment",
-                        populate: {
-                            path: "creator",
-                            model: "User",
-                        },
-                    }
-                );
-
+                const posts = await Post.find({ subject: subject._id })
+                .populate("creator")
+                .populate({
+                    path: "comments",
+                    model: "Comment",
+                    populate: {
+                        path: "creator",
+                        model: "User",
+                    },
+                })
+                .sort({ createDate: -1 });
                 return res.status(200).json({ success: true, data: posts });
             }
             catch (error) {
