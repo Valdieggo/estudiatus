b7e2d72979d353e2319cb8d7fc23cdb5352d946f
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-08-05 18:32:48 -0400

    Update PostsCard.js

--- components/Post/PostsCard.js
+++ components/Post/PostsCard.js
@@ -1,5 +1,5 @@
 import { VStack, Text, CardBody, IconButton, Box, Heading, Flex, Card, CardHeader, Image, CardFooter, Button, Avatar, Icon, Tag, TagLabel, TagCloseButton, HStack } from "@chakra-ui/react";
-import { ChatIcon, ArrowUpIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { ChatIcon, DownloadIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import LikePostButton from "./LikePostButton";
 import PopOptions from "./PopOptions";
 import { useRouter } from "next/router";
@@ -9,25 +9,32 @@ import axios from "axios";
 
 
 
-export default function PostsCard({ post,setAllPost, allPosts }) {
-    const {creator} = post
+export default function PostsCard({ post, setAllPost, allPosts }) {
+    const { creator } = post;
 
     let isCreatorId = false;
-    const {data: session,status}=useSession();
-    if (session) {
+    const { data: session, status } = useSession();
+    if (session && creator && creator._id) {
         isCreatorId = session.user.id === creator._id;
     }
-    const router = useRouter()
 
-    const handeleDeletPost =()=>{
-        axios.delete(`http://localhost:3000/api/post/delete/${post._id}`)
-        .then(res=>{
-            console.log(res.data.data)
-            setAllPost(allPosts.filter((post)=>post._id !== res.data.data._id))
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+    const isAdmin = session?.user.role === "admin";
+
+    const router = useRouter();
+
+    const handleDeletePost = () => {
+        if (isAdmin || isCreatorId) {
+            axios.delete(`http://localhost:3000/api/post/delete/${post._id}`)
+                .then(res => {
+                    console.log(res.data.data);
+                    setAllPost(allPosts.filter(post => post._id !== res.data.data._id));
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+            console.log("No tienes los permisos necesarios.");
+        }
     }
     return (
         <VStack key={post.id} margin={"5"} spacing={4} align="center">
@@ -51,7 +58,9 @@ export default function PostsCard({ post,setAllPost, allPosts }) {
                                 </Box>
                             </Flex>
                         )}
-                        <Button onClick={handeleDeletPost} >X</Button>
+                        {isAdmin || isCreatorId ? (
+                            <Button onClick={handleDeletePost}>X</Button>
+                        ) : null}
                         <PopOptions post={post} />
                     </Flex>
                 </CardHeader>
@@ -60,6 +69,9 @@ export default function PostsCard({ post,setAllPost, allPosts }) {
                         {post.content}
                     </Text>
                 </CardBody>
+                <Button as="a" href={`/api/File/download/${post.file}`}>
+                    Ver Documento
+                </Button>
 
                 <CardFooter
                     justify='space-between'
