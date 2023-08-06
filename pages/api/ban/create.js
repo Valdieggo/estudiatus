a350eeb0e1ad49b45d15c375c5b33117b294cc4f
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-08-06 00:44:41 -0400

    add mail logic

--- pages/api/ban/create.js
+++ pages/api/ban/create.js
@@ -3,7 +3,7 @@ import Ban from "../../../models/Ban";
 import User from "../../../models/User";
 import Report from "../../../models/Report";
 import mongoose from "mongoose";
-
+import mail from "../../../utils/mail";
 
 export default async function handler(req, res) {
     const { method } = req;
@@ -20,12 +20,17 @@ export default async function handler(req, res) {
                 const newBan = new Ban({ user, type, time, status, report });
                 await newBan.save();
                 // Actualiza el usuario en el campo de isBanned
-                await User.findByIdAndUpdate(user, { isBanned: true }, { session });
+                const bannedUser = await User.findByIdAndUpdate(user, { isBanned: true }, { session });
                 // Actualiza el reporte en el campo de estatus a "solved"
                 const ReportUpdate = await Report.findByIdAndUpdate(report, { status: 'solved' }, { session });
                 await session.commitTransaction();
                 session.endSession();
                 // Si todo va bien, se devuelve el nuevo ban creado
+                sendMail( bannedUser.email, bannedUser.username, "Sancion", `
+                    <p>Estimado ${bannedUser.username},</p>
+                    <p>Haz sido baneado lo lamentamos</p>
+                `);
+                
                 return res.status(201).json({ success: true, data: ReportUpdate });
                 
             }catch (error) {
