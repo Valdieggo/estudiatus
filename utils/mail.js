1f9c6bb599be01179752b5aef83d214c2c0de329
emersoftware <87254036+emersoftware@users.noreply.github.com>
2023-08-06 00:44:19 -0400

    add mail utils

---
+++ utils/mail.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+export default function sendMail(email, name, subject, htmlMessage) {
+    const token = process.env.PW;
+    const user = process.env.USER;
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // Cambiado a false para usar STARTTLS
+        auth: {
+            user: user,
+            pass: token,
+        },
+    });
+
+    const mailOptions = {
+        from: `Estudiatus <${user}>`,
+        to: email,
+        subject: subject,
+        html: htmlMessage,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+
+        if (error) {
+            return error
+        } else {
+            return info
+        }
+    });
+}
