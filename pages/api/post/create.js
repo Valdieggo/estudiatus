549aa80a27a78bd9f87df833af8029b3d01ed68e
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-08-09 02:33:10 -0400

    Update correo test

--- pages/api/post/create.js
+++ pages/api/post/create.js
@@ -1,58 +1,76 @@
 import { connectToDatabase } from "../../../utils/db";
-
 import Post from "../../../models/Post";
 import Subject from "../../../models/Subject";
 import User from "../../../models/User";
-
+import sendMail from "../../../utils/mail";
+import File from "../../../models/File";
 
 export default async function handler(req, res) {
     const { method } = req;
-    const { title, content,file, score, view, creator, subject,likes } = req.body;
+    const { title, content, file, score, view, creator, subject, likes } = req.body;
 
-    if (!title || !content ||!creator) {
+    if (!title || !content || !creator) {
         return res.status(400).json({ success: false, message: "Empty fields" });
     }
-
-
 
     await connectToDatabase();
 
     switch (method) {
         case "POST":
             try {
-                    const post = await Post.create({
-                        title,
-                        content,
-                        file,
-                        likes,
-                        score,
-                        view,
-                        creator,
-                        subject,
-                    });
-                    const postPopulated = await Post.findById(post._id)
-                        .populate({
-                            path: "creator",
-                            ref: "User",
-                        })
-                        .populate({
-                            path: "subject",
-                            ref: "Subject",
-                        })
+                const post = await Post.create({
+                    title,
+                    content,
+                    file,
+                    likes,
+                    score,
+                    view,
+                    creator,
+                    subject,
+                });
 
-                    await User.findByIdAndUpdate(creator, {
-                        $push: { posts: post._id }
-                    });
-                    await Subject.findByIdAndUpdate(subject, {
-                        $push: { posts: post._id }
+                const postPopulated = await Post.findById(post._id)
+                    .populate({
+                        path: "creator",
+                        ref: "User",
+                    })
+                    .populate({
+                        path: "subject",
+                        ref: "Subject",
+                    })
+                    .populate({
+                        path: "file",
+                        model: File,
                     });
 
-                    return res.status(200).json({ success: true, data: postPopulated });
-                }
-                catch (error) {
-                    return res.status(400).json({ success: false, message: error });
-                }
+                await User.findByIdAndUpdate(creator, {
+                    $push: { posts: post._id },
+                });
+
+                await Subject.findByIdAndUpdate(subject, {
+                    $push: { posts: post._id },
+                });
+
+                // Retrieve subject's subscribers
+                const subjectInfo = await Subject.findById(subject).populate("subscribers");
+                // console.log(subjectInfo)
+
+                subjectInfo.subscribers.forEach( (subscriber) => {
+                    console.log("Aqui 1")
+                    console.log(subscriber.email)
+
+                    sendMail(subscriber.email, subscriber.username, "Un nuevo post", `
+                        <p>Por ${subscriber.username},</p>
+                        <p>El nuevo post se titula "${title}" Fue creado por "${subjectInfo.subjectName}".</p>
+                        <p>Revisalo en <a href="${process.env.BASE_URL}/posts/${post._id}">Aqui</a>.</p>
+                    `);
+                });
+
+                return res.status(200).json({ success: true, data: postPopulated });
+            } catch (error) {
+                return res.status(400).json({ success: false, message: error });
+            }
         default:
-            return res.status(400).json({ success: false, message:error });
+            return res.status(400).json({ success: false, message: error });
     }
 }
