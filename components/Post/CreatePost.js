b684348fbd921d2d06344a8bf739a014974963d2
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-08-08 23:25:12 -0400

    up

--- components/Post/CreatePost.js
+++ components/Post/CreatePost.js
@@ -71,7 +71,7 @@ export default function CreatePost({ allPosts, setAllPosts, subject }) {
             const idfile = await Upload(file);
             setIsAddingPost(true);
             axios
-                .post(`${process.env.NEXT_PUBLIC_URL}:${process.env.PORT}/api/post/create`, {
+                .post(`${process.env.NEXT_PUBLIC_URL}:3000/api/post/create`, {
                     title: title,
                     content: content,
                     creator: session.user.id,
--- pages/api/post/getAllBysubject/[id].js
+++ pages/api/post/getAllBysubject/[id].js
@@ -35,7 +35,6 @@ export default async function handler(req, res) {
                             model: User,
                         },
                     })
-                    .populate("file")
                     .populate({
                         path: "file",
                         model: File,
