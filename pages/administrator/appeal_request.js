0811e4123e0ed19c260a96f6154a1eeb93517ab6
Geraldine <geraldine.aillon1901@alumnos.ubiobio.cl>
2023-08-09 02:50:10 -0400

    routefixadmin

--- pages/administrator/appeal_request.js
+++ pages/administrator/appeal_request.js
@@ -42,7 +42,7 @@ export default function appeal_request() {
   const [appeal,setAppeals]= useState([])
   const getAppeal = async () => {
       try {
-        const response = await axios.get("../api/appeal/getAll");
+        const response = await axios.get("http://localhost:3000/api/appeal/getAll");
         setAppeals(response.data);
       } catch (error) {
         console.log(error);
